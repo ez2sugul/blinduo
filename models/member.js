f9ce2b67088f8ec6de7ef85fa89ecf5928dc1b75
ez2sugul <seunghoon100@gmail.com>
2015-03-04 17:28:57 +0900

    validation added

--- models/member.js
+++ models/member.js
@@ -1,21 +1,85 @@
-"use strict"
+"use strict";
 
 module.exports = function(sequelize, DataTypes) {
 	var Member = sequelize.define('member', {
-		"username": {type : DataTypes.STRING, unique : true},
-		"my-company":{type : DataTypes.STRING, unique : false},
-		"email":{type : DataTypes.STRING, unique : true},
-		"birth-year":{type : DataTypes.INTEGER, unique : false},
-		"uppperlimit":{type : DataTypes.INTEGER, unique : false},
-		"lowerlimit":{type : DataTypes.INTEGER, unique : false},
-		"gender":{type : DataTypes.STRING, unique : false},
-		"my-character":{type : DataTypes.STRING, unique : false},
-		"prefer-character":{type : DataTypes.STRING, unique : false},
-		"my-charm":{type : DataTypes.STRING, unique : false},
-		"prefer-charm":{type : DataTypes.STRING, unique : false},
-		"exclude-company":{type : DataTypes.STRING, unique : false},
-		"my-religion":{type : DataTypes.STRING, unique : false},
-		"exclude-religion":{type : DataTypes.STRING, unique : false}
+		"username": {
+			type: DataTypes.STRING,
+			unique: true,
+			validate: {},
+			allowNull : false
+		},
+		"my-company": {
+			type: DataTypes.STRING,
+			unique: false,
+			allowNull : false
+		},
+		"email": {
+			type: DataTypes.STRING,
+			unique: true,
+			allowNull : false,
+			validate:{isEmail:true}
+		},
+		"birth-year": {
+			type: DataTypes.INTEGER,
+			unique: false,
+			allowNull : false,
+			validate: {isNumeric:true}
+		},
+		"uppperlimit": {
+			type: DataTypes.INTEGER,
+			unique: false,
+			allowNull : true,
+			defaultValue : 3,
+			validate: {isNumeric: true}
+		},
+		"lowerlimit": {
+			type: DataTypes.INTEGER,
+			unique: false,
+			allowNull : true,
+			defaultValue : 3,
+			validate: {isNumeric: true}
+		},
+		"gender": {
+			type: DataTypes.STRING,
+			unique: false,
+			allowNull: false,
+			validate: {isIn:[['M', 'F', 'm', 'f']]}
+		},
+		"my-character": {
+			type: DataTypes.STRING,
+			unique: false,
+			allowNull: false
+		},
+		"prefer-character": {
+			type: DataTypes.STRING,
+			unique: false,
+			allowNull: true
+		},
+		"my-charm": {
+			type: DataTypes.STRING,
+			unique: false,
+			allowNull: true
+		},
+		"prefer-charm": {
+			type: DataTypes.STRING,
+			unique: false,
+			allowNull: true
+		},
+		"exclude-company": {
+			type: DataTypes.STRING,
+			unique: false,
+			allowNull: true
+		},
+		"my-religion": {
+			type: DataTypes.STRING,
+			unique: false,
+			allowNull: false
+		},
+		"exclude-religion": {
+			type: DataTypes.STRING,
+			unique: false,
+			allowNull: true
+		}
 	});
 	return Member;
 };
